086b92fad54c9f17a96350a28826ce44bcbe9b35
Julian Weber <julianweberdev@gmail.com>
2019-09-14 18:38:38 +0200

    adds libmysqlclient-dev installation

--- scripts/install_mysql.sh
+++ scripts/install_mysql.sh
@@ -12,7 +12,7 @@ echo "mysql-server mysql-server/root_password password $PASSWORD" | sudo debconf
 echo "mysql-server mysql-server/root_password_again password $PASSWORD" | sudo debconf-set-selections
 
 # install package
-sudo apt-get -y install mysql-server-$MYSQL_VERSION
+sudo apt-get -y install mysql-server-$MYSQL_VERSION libmysqlclient-dev
 
 sudo sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/my.cnf
 sudo mysql -uroot -p$ROOT_PASSWORD -e 'USE mysql; UPDATE `user` SET `Host`="%" WHERE `User`="root" AND `Host`="localhost"; DELETE FROM `user` WHERE `Host` != "%" AND `User`="root"; FLUSH PRIVILEGES;'
